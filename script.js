d3d8a85e9be17fc3446f8e87b3edacd2136e8d87
Josafath <kevincc@live.com>
2020-12-02 15:22:16 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let change = document.querySelector(".change");
+    transform = {
+        0: "0",
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        6: "6",
+        7: "7",
+        8: "8",
+        9: "9",
+        10: "A",
+        11: "B",
+        12: "C",
+        13: "D",
+        14: "E",
+        15: "F",
+    }
+    change.addEventListener('click', () => {
+        let x = Math.floor(Math.random() * 256) + 1;
+        let y = Math.floor(Math.random() * 256) + 1;
+        let z = Math.floor(Math.random() * 256) + 1;
+        document.body.style.backgroundColor = `rgb(${x},${y},${z})`;
+        console.log(`rgb(${x},${y},${z})`);
+        document.body.style.transition = "2s";
+        hex = "#";
+        let value_x = Math.floor(x / 16);
+        let value_x_remainder = Math.floor(x % 16);
+        hex += transform[value_x];
+        hex += transform[value_x_remainder];
+
+        let value_y = Math.floor(y / 16);
+        let value_y_remainder = Math.floor(y % 16);
+        hex += transform[value_y];
+        hex += transform[value_y_remainder];
+
+        let value_z = Math.floor(z / 16);
+        let value_z_remainder = Math.floor(z % 16);
+        hex += transform[value_z];
+        hex += transform[value_z_remainder];
+
+        document.querySelector(".data-color").innerHTML = `Background Color: ${hex}`;
+
+    })
+});
