1826c87cc10695589c03500714ced0ca16673805
Josafath <kevincc@live.com>
2021-05-12 12:44:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     let change = document.querySelector(".change");
-    transform = {
+    hexa_table = {
         0: "0",
         1: "1",
         2: "2",
@@ -23,23 +23,22 @@ document.addEventListener('DOMContentLoaded', () => {
         let y = Math.floor(Math.random() * 256) + 1;
         let z = Math.floor(Math.random() * 256) + 1;
         document.body.style.backgroundColor = `rgb(${x},${y},${z})`;
-        console.log(`rgb(${x},${y},${z})`);
         document.body.style.transition = "2s";
         hex = "#";
         let value_x = Math.floor(x / 16);
         let value_x_remainder = Math.floor(x % 16);
-        hex += transform[value_x];
-        hex += transform[value_x_remainder];
+        hex += hex_table[value_x];
+        hex += hex_table[value_x_remainder];
 
         let value_y = Math.floor(y / 16);
         let value_y_remainder = Math.floor(y % 16);
-        hex += transform[value_y];
-        hex += transform[value_y_remainder];
+        hex += hex_table[value_y];
+        hex += hex_table[value_y_remainder];
 
         let value_z = Math.floor(z / 16);
         let value_z_remainder = Math.floor(z % 16);
-        hex += transform[value_z];
-        hex += transform[value_z_remainder];
+        hex += hex_table[value_z];
+        hex += hex_table[value_z_remainder];
 
         document.querySelector(".data-color").innerHTML = `Background Color: ${hex}`;
 
